253bb0aeef19b9fbb7984d89402feca510f4a225
Robert Schwartz <bschwartz23@gmail.com>
2021-08-07 17:42:35 -0700

    set up App.js REACT Routes and Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState } from "react";
+import MainNavigation from "./components/layout/MainNavigation";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+
+// Pages
+import Homepage from "./pages/Homepage";
+import DualFinder from "./pages/DualFinder";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import SignUp from "./pages/Contact";
+
+// Components
+import Navbar from "./components/layout/Navbar.js";
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+	return (
+		<div>
+			<Container maxWidth="xl" disableGutters={true}>
+				<div>
+					<Navbar />
+					<Switch>
+						<Route path="/">
+							<Homepage />
+						</Route>
+						<Route path="" exact>
+							<Homepage />
+						</Route>
+						<Route path="/DualFinder" exact>
+							<DualFinder />
+						</Route>
+						<Route path="/Dashboard" exact>
+							<Dashboard />
+						</Route>
+						<Route path="/Login" exact>
+							<Login />
+						</Route>
+						<Route path="/SignUp" exact>
+							<SignUp />
+						</Route>
+					</Switch>
+				</div>
+			</Container>
+		</div>
+	);
 }
 
 export default App;
