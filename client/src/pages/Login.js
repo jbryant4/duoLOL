7baec4a0a504a61e182b43dc8152a3d27f14c0d0
Nathan da Silva <pacsmacck@gmail.com>
2021-08-10 07:02:19 -0700

    login video and form v1

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,17 @@
 // React imports
 import React, { useState } from "react";
 import { useMutation } from "@apollo/client";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 // Utility imports
 import { LOGIN } from "../utils/mutations";
 import Auth from "../utils/auth";
+
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+import loginVideo from "../assets/videos/1.mp4"
+
+
 
 // Login Function
 function Login(props) {
@@ -33,42 +40,58 @@ function Login(props) {
 	};
 
 	return (
-		<div className="container my-1">
-			<Link to="/signup">← Create an account here!</Link>
-
-			<h2>Login</h2>
-			<form onSubmit={handleFormSubmit}>
-				<div className="flex-row space-between my-2">
-					<label htmlFor="email">Email address:</label>
-					<input
-						placeholder="youremail@test.com"
-						name="email"
-						type="email"
-						id="email"
-						onChange={handleChange}
-					/>
-				</div>
-				<div className="flex-row space-between my-2">
-					<label htmlFor="pwd">Password:</label>
-					<input
-						placeholder="******"
-						name="password"
-						type="password"
-						id="pwd"
-						onChange={handleChange}
-					/>
-				</div>
-				{error ? (
-					<div>
-						<p className="error-text">The provided credentials are incorrect</p>
+		<div className="container my-1" >
+			<video
+				autoPlay loop muted
+				style={{
+					position: "absolute",
+					width: "100%",
+					left: "50%",
+					top: "50%",
+					height: "100%",
+					objectFit: "cover",
+					transform: "translate(-50%, -50%)",
+					zIndex: "-1"
+				}}
+			>
+				<source src={loginVideo} type="video/mp4" />
+			</video>
+			<main className="loginSignupForm">
+				<h1 className="loginFont">Login</h1>
+				<form onSubmit={handleFormSubmit}>
+					<div className="flex-row space-between my-2">
+						<label htmlFor="email"></label>
+						<TextField id="outlined-basic" label="Email" variant="outlined"
+							placeholder="email"
+							name="email"
+							type="email"
+							id="email"
+							onChange={handleChange}
+						/>
 					</div>
-				) : null}
-				<div className="flex-row flex-end">
-					<button type="submit">Submit</button>
-				</div>
-			</form>
+					<div className="flex-row space-between my-2">
+						<label htmlFor="pwd"></label>
+						<TextField id="outlined-basic" label="Password" variant="outlined"
+							placeholder="password"
+							name="password"
+							type="password"
+							id="pwd"
+							onChange={handleChange}
+						/>
+					</div>
+					{error ? (
+						<div>
+							<p className="error-text">The provided credentials are incorrect</p>
+						</div>
+					) : null}
+					<div className="flex-row flex-end">
+						<button type="submit">Login</button>
+					</div>
+				</form>
+			</main>
 		</div>
-	);
+
+	);					
 }
 
 //export Login
