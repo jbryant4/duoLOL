cb5553dbd493b4868889d581b27712d57ce18e62
Robert Schwartz <bschwartz23@gmail.com>
2021-08-11 15:16:14 -0700

    create ComponentList component for Champion Components Page

--- client/src/pages/AboutChampion.js
+++ client/src/pages/AboutChampion.js
@@ -1,19 +1,26 @@
 // React
 import React from "react";
 
+// Material UI
+import { Container, Box } from "@material-ui/core";
+
 //Components
 import SimpleSelect from "../components/SimpleSelect";
-
-
-
+import ComponentList from "../components/ComponentList";
 
 // AboutChampion
 function AboutChampion(props) {
 	return (
 		<section>
-			<h1>Champions</h1>
-			<h2>Select a Champion to Learn More</h2>
-			<SimpleSelect />
+			<Container maxWidth="sm">
+				<h1>Champions</h1>
+				<h2>Select a Champion to Learn More</h2>
+				<SimpleSelect />
+				<ComponentList />
+				<Box color="text.primary" clone>
+					<h1>Content goes here</h1>
+				</Box>
+			</Container>
 		</section>
 	);
 }
