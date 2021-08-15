376e720acfb9a982989fd3dc5c5a07fd96e92669
Robert Schwartz <bschwartz23@gmail.com>
2021-08-15 15:18:39 -0700

    Create Friends List Component with sample text.  testFriends array in Homepage.js

---
+++ client/src/components/FriendList/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+//Material Components
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles({
+	buttonClass: {
+		backgroundColor: "purple",
+		variant: "outlined",
+		color: "white",
+		padding: "5px",
+		margin: "10px",
+	},
+	friend: {
+		margin: "10px",
+		padding: "10px",
+		border: "1px solid black",
+		borderRadius: "10% 05%",
+		backgroundColor: "whitesmoke",
+		fontWeight: "bold",
+		fontSize: "1rem",
+	},
+});
+
+// Friend List
+export default function FriendList({ name }) {
+	// use Material styles
+	const classes = useStyles();
+	const friendsList = name;
+	console.log(friendsList);
+	return (
+		<Grid>
+			<div>
+				{friendsList &&
+					friendsList.map((friendName) => (
+						<div key={friendName} className={classes.friend}>
+							{friendName}
+							<Button
+								className={classes.buttonClass}
+								// onClick={() => go to chat)}
+							>
+								Chat
+							</Button>
+						</div>
+					))}
+			</div>
+		</Grid>
+	);
+}
