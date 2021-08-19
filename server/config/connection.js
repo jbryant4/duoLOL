f9c44a2b4a7a323a36d960e37be948428a5e9a24
CBear <chebert222@gmail.com>
2021-08-19 11:56:43 -0700

    fixes for pull

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/duoLOL3', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/duoLOL2', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
